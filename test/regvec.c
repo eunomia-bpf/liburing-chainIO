fd7ba556d501078d8c48fadc35bc340e21143b52
Pavel Begunkov <asml.silence@gmail.com>
2025-03-07 10:58:03 +0000

    regvec:
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- test/regvec.c
+++ test/regvec.c
@@ -9,6 +9,7 @@
 #include <poll.h>
 #include <sys/eventfd.h>
 #include <sys/resource.h>
+#include <pthread.h>
 
 #include "helpers.h"
 #include "liburing.h"
@@ -27,7 +28,7 @@ struct buf_desc {
 	bool			rw;
 };
 
-#define BUFFER_SIZE	(4096 * 16)
+#define BUFFER_SIZE	(4096 * 32)
 #define BUF_BASE_IDX	1
 
 static void probe_support(void)
@@ -118,46 +119,66 @@ static void init_buffers(struct buf_desc *bd, size_t size)
 	}
 }
 
-static void verify_data(struct buf_desc *bd, struct iovec *wr_vecs, int nr_iovec,
-			int pipefd)
+static int verify_data(struct buf_desc *bd, struct iovec *wr_vecs, int nr_iovec,
+			int fd)
 {
 	int iov_idx, ret;
 
 	for (iov_idx = 0; iov_idx < nr_iovec; iov_idx++) {
 		struct iovec *vec = &wr_vecs[iov_idx];
 		size_t seg_size = vec->iov_len;
+		size_t read_bytes = 0;
 
-		ret = read(pipefd, bd->buf_rd, seg_size);
-		if (ret != seg_size) {
-			fprintf(stderr, "read error %i", ret);
-			exit(1);
+		while (1) {
+			ret = read(fd, bd->buf_rd + read_bytes, seg_size - read_bytes);
+			if (ret < 0) {
+				fprintf(stderr, "read error %i", ret);
+				return 1;
+			}
+			read_bytes += ret;
+			if (read_bytes == seg_size)
+				break;
+			if (ret == 0) {
+				fprintf(stderr, "can't read %i", ret);
+				return 2;
+			}
 		}
 
 		ret = memcmp(bd->buf_rd, vec->iov_base, seg_size);
 		if (ret != 0) {
 			fprintf(stderr, "data mismatch %i\n", ret);
-			exit(1);
+			return 3;
 		}
 	}
+	return 0;
 }
 
-static int test_rw(struct buf_desc *bd, struct iovec *vecs, int nr_vec,
-		      bool expect_fail, size_t total_len)
+struct verify_data {
+	struct buf_desc *bd;
+	struct iovec *vecs;
+	int nr_vec;
+	int fd;
+};
+
+static void *verify_thread_cb(void *data)
+{
+	struct verify_data *vd = data;
+	int ret;
+
+	ret = verify_data(vd->bd, vd->vecs, vd->nr_vec, vd->fd);
+	return (void *)(unsigned long)ret;
+}
+
+static int test_rw(struct buf_desc *bd, struct iovec *vecs, int nr_vec, int fd_wr)
 {
 	unsigned buf_idx = bd->buf_idx + 1;
 	struct io_uring *ring = &bd->ring;
 	struct io_uring_sqe *sqe;
 	struct io_uring_cqe *cqe;
-	int pipefd[2];
 	int ret;
 
-	if (pipe(pipefd) != 0) {
-		perror("pipe");
-		exit(1);
-	}
-
 	sqe = io_uring_get_sqe(ring);
-	io_uring_prep_writev(sqe, pipefd[1], vecs, nr_vec, 0);
+	io_uring_prep_writev(sqe, fd_wr, vecs, nr_vec, 0);
 	if (bd->fixed)
 		sqe->buf_index = buf_idx;
 
@@ -175,45 +196,27 @@ static int test_rw(struct buf_desc *bd, struct iovec *vecs, int nr_vec,
 	printf("cqe: data %i, res %i\n", (int)cqe->user_data, (int)cqe->res);
 	ret = cqe->res;
 	io_uring_cqe_seen(ring, cqe);
-
-	if (ret != total_len) {
-		if (!expect_fail)
-			fprintf(stderr, "invalid cqe %i, expected %lu\n",
-					 ret, (unsigned long)total_len);
-		return ret;
-	}
-
-	verify_data(bd, vecs, nr_vec, pipefd[0]);
-	close(pipefd[0]);
-	close(pipefd[1]);
-	return 0;
+	return ret;
 }
 
-static int test_sendzc(struct buf_desc *bd, struct iovec *vecs, int nr_vec,
-		       bool expect_fail, size_t total_len)
+static int test_sendzc(struct buf_desc *bd, struct iovec *vecs, int nr_vec, int fd_wr)
 {
 	unsigned buf_idx = bd->buf_idx + 1;
 	struct io_uring *ring = &bd->ring;
 	struct io_uring_sqe *sqe;
 	struct io_uring_cqe *cqe;
-	struct sockaddr_storage addr;
-	int sock_serv, sock_client;
 	int ret, cqe_ret, more;
 	struct msghdr msghdr;
-
-	ret = t_create_socketpair_ip(&addr, &sock_client, &sock_serv,
-					true, true, false, true, "::1");
-	if (ret) {
-		fprintf(stderr, "sock prep failed %d\n", ret);
-		return 1;
-	}
 
 	memset(&msghdr, 0, sizeof(msghdr));
 	msghdr.msg_iov = vecs;
 	msghdr.msg_iovlen = nr_vec;
 
 	sqe = io_uring_get_sqe(ring);
-	io_uring_prep_sendmsg_zc_fixed(sqe, sock_client, &msghdr, 0, buf_idx);
+	if (bd->fixed)
+		io_uring_prep_sendmsg_zc_fixed(sqe, fd_wr, &msghdr, 0, buf_idx);
+	else
+		io_uring_prep_sendmsg_zc(sqe, fd_wr, &msghdr, 0);
 
 	ret = io_uring_submit(ring);
 	if (ret != 1) {
@@ -239,26 +242,26 @@ static int test_sendzc(struct buf_desc *bd, struct iovec *vecs, int nr_vec,
 		}
 		io_uring_cqe_seen(ring, cqe);
 	}
-
-	if (cqe_ret != total_len) {
-		if (!expect_fail)
-			fprintf(stderr, "invalid cqe %i, expected %lu\n",
-					 cqe_ret, (unsigned long)total_len);
-		return cqe_ret;
-	}
-
-	verify_data(bd, vecs, nr_vec, sock_serv);
-	close(sock_client);
-	close(sock_serv);
-	return 0;
+	return cqe_ret;
 }
-
 
 static int test_vec(struct buf_desc *bd, struct iovec *vecs, int nr_vec,
 		    bool expect_fail)
 {
+	struct sockaddr_storage addr;
+	int sock_server, sock_client;
+	struct verify_data vd;
 	size_t total_len = 0;
-	int i;
+	int i, ret;
+	void *verify_res;
+	pthread_t th;
+
+	ret = t_create_socketpair_ip(&addr, &sock_client, &sock_server,
+					true, true, false, true, "::1");
+	if (ret) {
+		fprintf(stderr, "sock prep failed %d\n", ret);
+		return 1;
+	}
 
 	for (i = 0; i < bd->size; i++)
 		bd->buf_wr[i] = i;
@@ -267,10 +270,42 @@ static int test_vec(struct buf_desc *bd, struct iovec *vecs, int nr_vec,
 	for (i = 0; i < nr_vec; i++)
 		total_len += vecs[i].iov_len;
 
-	if (bd->rw)
-		return test_rw(bd, vecs, nr_vec, expect_fail, total_len);
+	vd.bd = bd;
+	vd.vecs = vecs;
+	vd.nr_vec = nr_vec;
+	vd.fd = sock_server;
 
-	return test_sendzc(bd, vecs, nr_vec, expect_fail, total_len);
+	if (!expect_fail) {
+		ret = pthread_create(&th, NULL, verify_thread_cb, &vd);
+		if (ret) {
+			fprintf(stderr, "pthread_create failed %i\n", ret);
+			return ret;
+		}
+	}
+
+	if (bd->rw)
+		ret = test_rw(bd, vecs, nr_vec, sock_client);
+	else
+		ret = test_sendzc(bd, vecs, nr_vec, sock_client); 
+
+	if (ret != total_len) {
+		if (!expect_fail)
+			fprintf(stderr, "invalid cqe %i, expected %lu\n",
+					 ret, (unsigned long)total_len);
+		return 1;
+	}
+
+	if (!expect_fail) {
+		pthread_join(th, &verify_res);
+		ret = (int)(unsigned long)verify_res;
+		if (ret) {
+			fprintf(stderr, "verify failed  %i\n", ret);
+			return 1;
+		}
+	}
+	close(sock_client);
+	close(sock_server);
+	return ret;
 }
 
 struct work {
@@ -296,99 +331,161 @@ static int test_sequence(struct buf_desc *bd, unsigned nr, struct work *ws)
 
 static void test(struct buf_desc *bd)
 {
+	void *p = bd->buf_wr;
+	const int page_sz = 4096;
 	int ret;
+	struct iovec iov_page =		{ .iov_base = p,
+					  .iov_len = page_sz, };
+	struct iovec iov_inner =	{ .iov_base = p + 1,
+					  .iov_len = 3, };
+	struct iovec iov_maxbvec =	{ .iov_base = p + page_sz - 1,
+					  .iov_len = page_sz + 2, };
+	struct iovec iov_big =		{ .iov_base = p,
+					  .iov_len = page_sz * 12 + 33, };
+	struct iovec iov_big_unalign =	{ .iov_base = p + 10,
+					  .iov_len = page_sz * 7 + 41, };
+	struct iovec iov_full =		{ .iov_base = p,
+					  .iov_len = BUFFER_SIZE, };
+	struct iovec iov_full_unalign = { .iov_base = p + 1,
+					  .iov_len = BUFFER_SIZE - 1, };
 	struct iovec vecs[] = {
-		{ .iov_base = bd->buf_wr, .iov_len = 4096, },
-		{ .iov_base = bd->buf_wr, .iov_len = 4096 * 2 + 12, },
-		{ .iov_base = bd->buf_wr + 1, .iov_len = 3, },
-		{ .iov_base = bd->buf_wr + 1, .iov_len = 4096 * 5, },
+		iov_page,
+		iov_big,
+		iov_inner,
+		iov_big_unalign,
+		iov_big_unalign,
 	};
-	struct iovec vecs2[] = {
-		{ .iov_base = bd->buf_wr, .iov_len = 4096, },
-		{ .iov_base = bd->buf_wr, .iov_len = 4096, },
-		{ .iov_base = bd->buf_wr, .iov_len = 4096, },
-	};
+	struct iovec vecs_basic[] = { iov_page, iov_page, iov_page };
+	struct iovec vecs_full[] = { iov_full, iov_full, iov_full };
+	struct iovec vecs_full_unalign[] = { iov_full_unalign, iov_full_unalign,
+					     iov_full_unalign };
+	struct iovec vecs_maxsegs[] = { iov_maxbvec, iov_maxbvec, iov_maxbvec,
+				      iov_maxbvec, iov_maxbvec, iov_maxbvec};
 
-	ret = test_sequence(bd, 2, (struct work[]) {
-			{ &vecs[0], 1},
-			{ &vecs[0], 1}});
+	ret = test_sequence(bd, 1, (struct work[]) {
+			{ &iov_page, 1 },
+			{ vecs, 1 }});
 	if (ret) {
 		fprintf(stderr, "seq failure: basic aligned, %i\n", ret);
 		exit(1);
 	}
 
 	ret = test_sequence(bd, 2, (struct work[]) {
-			{ &vecs[1], 1},
-			{ &vecs[1], 1}});
+			{ vecs, 1 },
+			{ vecs, 1 }});
+	if (ret) {
+		fprintf(stderr, "seq failure: basic aligned, %i\n", ret);
+		exit(1);
+	}
+
+	ret = test_sequence(bd, 2, (struct work[]) {
+			{ vecs + 1, 1 },
+			{ vecs + 1, 1 }});
 	if (ret) {
 		fprintf(stderr, "seq failure: multi page buffer, %i\n", ret);
 		exit(1);
 	}
 
 	ret = test_sequence(bd, 2, (struct work[]) {
-			{ &vecs[2], 1},
-			{ &vecs[2], 1}});
+			{ vecs + 2, 1 },
+			{ vecs + 2, 1 }});
 	if (ret) {
 		fprintf(stderr, "seq failure: misaligned buffer, %i\n", ret);
 		exit(1);
 	}
 
 	ret = test_sequence(bd, 2, (struct work[]) {
-			{ &vecs[3], 1},
-			{ &vecs[3], 1}});
+			{ vecs + 3, 1 },
+			{ vecs + 3, 1 }});
 	if (ret) {
 		fprintf(stderr, "seq failure: misaligned multipage buffer, %i\n", ret);
 		exit(1);
 	}
 
 	ret = test_sequence(bd, 2, (struct work[]) {
-			{ &vecs[0], 1},
-			{ &vecs[3], 1}});
+			{ vecs, 1 },
+			{ vecs + 3, 1 }});
 	if (ret) {
 		fprintf(stderr, "seq failure: realloc + increase bvec, %i\n", ret);
 		exit(1);
 	}
 
 	ret = test_sequence(bd, 2, (struct work[]) {
-			{ &vecs[3], 1},
-			{ &vecs[0], 1}});
+			{ vecs + 3, 1 },
+			{ vecs + 0, 1 }});
 	if (ret) {
 		fprintf(stderr, "seq failure: realloc + decrease bvec, %i\n", ret);
 		exit(1);
 	}
 
 	ret = test_sequence(bd, 2, (struct work[]) {
-			{ &vecs[0], 4},
-			{ &vecs[0], 4}});
+			{ vecs, 4 },
+			{ vecs, 4 }});
 	if (ret) {
 		fprintf(stderr, "seq failure: multisegment, %i\n", ret);
 		exit(1);
 	}
 
 	ret = test_sequence(bd, 3, (struct work[]) {
-			{ &vecs[0], 2},
-			{ &vecs[0], 3},
-			{ &vecs[0], 4}});
+			{ vecs, 2 },
+			{ vecs, 3 },
+			{ vecs, 4 }});
 	if (ret) {
 		fprintf(stderr, "seq failure: multisegment 2, %i\n", ret);
 		exit(1);
 	}
 
 	ret = test_sequence(bd, 3, (struct work[]) {
-			{ &vecs2[0], 1},
-			{ &vecs2[0], 2},
-			{ &vecs2[0], 3}});
+			{ vecs_basic, 1 },
+			{ vecs_basic, 2 },
+			{ vecs_basic, 3 }});
 	if (ret) {
 		fprintf(stderr, "seq failure: increase iovec, %i\n", ret);
 		exit(1);
 	}
 
 	ret = test_sequence(bd, 3, (struct work[]) {
-			{ &vecs2[0], 3},
-			{ &vecs2[0], 2},
-			{ &vecs2[0], 1}});
+			{ vecs_basic, 3 },
+			{ vecs_basic, 2 },
+			{ vecs_basic, 1 }});
 	if (ret) {
 		fprintf(stderr, "seq failure: decrease iovec, %i\n", ret);
+		exit(1);
+	}
+
+	ret = test_sequence(bd, 3, (struct work[]) {
+			{ vecs_full, 1 },
+			{ vecs_full, 1 },
+			{ vecs_full, 3 }});
+	if (ret) {
+		fprintf(stderr, "seq failure: full size, %i\n", ret);
+		exit(1);
+	}
+
+	ret = test_sequence(bd, 3, (struct work[]) {
+			{ vecs_full_unalign, 1 },
+			{ vecs_full_unalign, 1 },
+			{ vecs_full_unalign, 3 }});
+	if (ret) {
+		fprintf(stderr, "seq failure: full size unsigned, %i\n", ret);
+		exit(1);
+	}
+
+	ret = test_sequence(bd, 3, (struct work[]) {
+			{ vecs_maxsegs, 1 },
+			{ vecs_maxsegs, 2 },
+			{ vecs_maxsegs, 3 }});
+	if (ret) {
+		fprintf(stderr, "seq failure: overestimated segments, %i\n", ret);
+		exit(1);
+	}
+
+	ret = test_sequence(bd, 3, (struct work[]) {
+			{ vecs_maxsegs, 6 },
+			{ vecs_maxsegs, 6 },
+			{ vecs_maxsegs, 6 }});
+	if (ret) {
+		fprintf(stderr, "seq failure: overestimated segments 2, %i\n", ret);
 		exit(1);
 	}
 }
